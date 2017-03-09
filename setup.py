a5b350a6ea15abda3df182f9fb1e3a95d3a52398
Gabriel Oliveira <dev@gabrieloliveira.net>
2017-03-09 09:17:30 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='EgoiApi',
+    version='1.0',
+    description='Egoi API Python Wrapper',
+    author='E-goi, Gabriel Oliveira',
+    author_email='gabriel@gabrieloliveira.net',
+    packages=['EgoiApi']
+    )
